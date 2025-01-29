29c288b7d2ab7941c0c0602062ae12249e76746b
Ambuj-Singh07 <ambuj1393@gmail.com>
2025-01-29 19:09:07 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all js code as newer version
+
+//alert(3 + 3)//we are using in node js,not browser
+
+console.log(3 + 3);
+
+console.log("Ambuj Kumar singh");
+
+let name= "Ambuj Kumar Singh"
+let age = 18
+
+let isLoggedIn= true
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => Standalone value // null is object
+// undefined =>
+// symbol => unique
+
+//Object
+
+console.log(typeof null); // Output Null is object
+console.log(typeof undefined);// Undefined
