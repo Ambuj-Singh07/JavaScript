99fa96f7f40814d6831b048d32d130a05f3f8a8b
Ambuj-Singh07 <ambuj1393@gmail.com>
2025-01-29 19:02:07 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,12 @@
+const accountId = 144345
+let accountEmail = "ambuj@google.com"
+var accountPassword = "12345"
+accountCity ="Bihar"
+
+accountEmail="hche@hc.com"
+accountPassword = "123456798"
+accountCity="Gujrat"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
