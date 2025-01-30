59f942092e1ed2eaa0a17c40455b277a0ef6f31e
Ambuj-Singh07 <ambuj1393@gmail.com>
2025-01-30 18:37:59 +0530

    Add files via upload

---
+++ 04_comparision.js
@@ -0,0 +1,11 @@
+console.log(2 > 1);
+
+console.log("2" > 1);
+console.log("02" > 1);// number alerady convert in number data types
+
+console.log(null > 0);//Output = False
+console.log(null == 0);// Output = false
+console.log(null >= 0);// Output = true // that`s why null >= 0 is true and null > 0 is false;
+
+
+
